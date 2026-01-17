248954a150047b3ef282a7be912221797b905f3c
severity1 <johnreilly.pospos@gmail.com>
2026-01-17 19:23:03 +1300

    refactor: Format import order per gofmt (BLUE)
    - Reorder imports alphabetically within groups
    - All tests still passing

--- imports_test.go
+++ imports_test.go
@@ -4,9 +4,9 @@ import (
 	"testing"
 
 	// Charmbracelet dependencies
-	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/bubbles/textarea"
 	"github.com/charmbracelet/bubbles/viewport"
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 
 	// SDK dependency
